1cad70b758427bf453a7395cbeae5613772c82da
Totobird <>
2020-09-18 12:21:46 -0400

    Import Test

--- icons-svg.js
+++ icons-svg.js
@@ -1,4 +1,4 @@
-import { $ } from 'https://code.jquery.com/jquery-3.5.1.min.js';
+//import { $ } from 'https://code.jquery.com/jquery-3.5.1.min.js';
 
 $(document).ready(function() {
     console.log('Hello World!');
